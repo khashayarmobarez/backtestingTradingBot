9cdabe8242f45f2122d5dfcd8e8d4e38d43a5570
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-01 17:36:54 +0330

    Implement code changes to enhance functionality and improve performance

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-print("Libraries are working!")
+# Load CSV without header
+data = pd.read_csv("gold_data.csv", header=None)
 
-# create a fake price series
-prices = pd.Series(np.random.randn(100).cumsum())
+# Assign proper column names
+data.columns = ["Date", "Time", "Open", "High", "Low", "Close", "Volume"]
 
-# plot
-prices.plot(title="Fake Price Data")
+# Combine Date + Time into one datetime column
+data["DateTime"] = pd.to_datetime(data["Date"] + " " + data["Time"])
+
+# Set index
+data.set_index("DateTime", inplace=True)
+
+# Example: Simple moving average strategy
+data["SMA_10"] = data["Close"].rolling(window=10).mean()
+data["SMA_30"] = data["Close"].rolling(window=30).mean()
+
+# Generate signals
+data["Signal"] = 0
+data.loc[data["SMA_10"] > data["SMA_30"], "Signal"] = 1
+data.loc[data["SMA_10"] < data["SMA_30"], "Signal"] = -1
+
+# Plot
+plt.figure(figsize=(12,6))
+plt.plot(data.index, data["Close"], label="Gold Price")
+plt.plot(data.index, data["SMA_10"], label="SMA 10")
+plt.plot(data.index, data["SMA_30"], label="SMA 30")
+plt.legend()
 plt.show()
